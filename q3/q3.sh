57980eff7b1f25856d65b8f5157b588eb01a8053
Utkarsh <utkarshindolia@cse.iitb.ac.in>
2020-09-04 22:56:49 +0530

    removed comments

--- q3/q3.sh
+++ q3/q3.sh
@@ -1,17 +1,7 @@
-#grep '[[:alpha:]]\+-[[:digit:]]\{3\}-[[:digit:]]\{8\} \|$' input3
-#grep -o ' \?[[:alpha:]]\+-[[:digit:]]\{3\}-[[:digit:]]\{8\}' input3
-#sed -n -r 's/.*(( |^)[[:alpha:]]+-[[:digit:]]{3}-[[:digit:]]{8}( |$)).*/\1/p' input3
-
 grep -oE '( |^)[[:alpha:]]+-[[:digit:]]{3}-[[:digit:]]{8}( |$)' $1 \
 | sed '/-00000000/d; s/-/ /' > temp1
 
-
 sed -r '1d; s/ *\t+ */-/g; s/ +/_/g' STD_Codes_list > temp2
-
-#cat temp2
-#echo -e "\n======="
-#cat temp1
-#echo "======="
 
 awk -F"[ -]" '
 {
@@ -31,8 +21,5 @@ awk -F"[ -]" '
 			}
 		}
 }' temp2 temp1 | sed 's/_/ /g'
-#cat temp3
-#cat temp1
-#echo "==========="
-#cat temp2
+
 rm temp1 temp2
