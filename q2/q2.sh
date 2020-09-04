d0f1750335ed300760a18741977008dcf3547332
Ayush <ayushsharma1198@gmail.com>
2020-09-04 23:05:24 +0530

    q2 updated and passed

--- q2/q2.sh
+++ q2/q2.sh
@@ -1,17 +1,17 @@
 INFILE=$1
 OUTFILE="url"
+sed -i -e 's/   */ /g' $INFILE
+sed -i -e 's/\t\t*/ /g' $INFILE
 sed -i -e 's/https:/ https:/g' $INFILE
 sed -i -e 's/http:/ http:/g' $INFILE
 sed -r -i 's/\(/\(/g;s/\/\)/\/ \)/g' $INFILE
-sed -i -e 's/   */ /g' $INFILE
-sed -i -e 's/\t\t*/ /g' $INFILE
 awk '{
 	for(i=1;i<=NF;i++){
 		if($i ~ /https:\/\/www.cse.iitb.ac.in\/~[a-z][a-z]*/ || $i ~ /http:\/\/www.cse.iitb.ac.in\/~[a-z][a-z]*/){
 			print $i
 		}
 	}
-}' $INFILE | sed 's/.*\(http[A-Za-z]*\)/\1/' > $OUTFILE
+}' $INFILE > $OUTFILE
 awk '{
 	for(i=1;i<=NF;i++){
 		if($i ~ /^https:\/\/.*/ || $i ~ /^http:\/\/.*/ || $i ~ /.*(.com|.in|.org|.net|.co|.us|.edu|.gov)\/.*/){
