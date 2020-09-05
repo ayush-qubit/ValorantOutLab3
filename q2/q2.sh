c4cf82d52fe21d6f16f579d80bc6d2ad6738bf15
“nishhhkam” <bharatpanwar777@gmail.com>
2020-09-05 10:01:15 +0530

    q2 not done

--- q2/q2.sh
+++ q2/q2.sh
@@ -1,7 +1,5 @@
 grep -oE 'http(s?)://www.cse.iitb.ac.in/~[a-z]+ ' $1 > url
 
-#sed -r 's/\t+/ /g; s/ +/ /g' input1 | grep -E 'http(s?)://[^ ]+ '
-
 sed -r 's/\t+/ /g' $1 \
 | sed -r 's/http(s?):\/\/[^ )]+ //g' \
 | sed -r 's/\(http(s?):\/\/[^\)]+\)/( )/g' \
@@ -10,9 +8,3 @@ sed -r 's/\t+/ /g' $1 \
 | sed -r 's/ +/ /g' > $1
 
 
-#sed -r 's/\t+/ /g' input1 \
-#| sed -r 's/http(s?):\/\/[^ )]+ //g' \
-#| sed -r 's/ [^ ]+(\.net|\.in)\/[^ ]+ / /g' \
-#| sed -r '[^\(]+(\.com|\.in|\.org|\.net|\.co|\.us|\.edu|\.gov)/[^\)]+'
-
-#sed -ri 's/\t+/ /g; s/ +/ /g; s/http(s?):\/\/[^ ]+ //g' $1
