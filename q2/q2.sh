6c8ffd6edf26b162402cd6eb8439a8e8515ef02d
Bharat Panwar <31369524+nishhhkam@users.noreply.github.com>
2020-09-05 13:18:22 +0530

    q2 done!!!!

--- q2/q2.sh
+++ q2/q2.sh
@@ -1,4 +1,4 @@
-#input file that contains details about classmates
+
 grep -oE 'http(s?)://www.cse.iitb.ac.in/~[a-z]+ ' $1 > url
 
 sed -r 's/\t+/ /g' $1 \
@@ -6,7 +6,7 @@ sed -r 's/\t+/ /g' $1 \
 | sed -r 's/\(http(s?):\/\/[^\)]+\)/( )/g' \
 | sed -r 's/ [^ ]+(\.com|\.in|\.org|\.net|\.co|\.us|\.edu|\.gov)\/[^ ]+ / /g' \
 | sed -r 's/[^\(]+(\.com|\.in|\.org|\.net|\.co|\.us|\.edu|\.gov)\/[^\)]+/ /g' \
-#output file that contain webpage links of our classmates.
+
 | sed -r 's/ +/ /g' > $1
 
 
