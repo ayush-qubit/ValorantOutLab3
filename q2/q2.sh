fa47890322d321a4229c258fa5735be4db44c2e2
utkarsh92 <utkarshindolia@cse.iitb.ac.in>
2020-09-23 17:21:43 +0530

    fixed q2

--- q2/q2.sh
+++ q2/q2.sh
@@ -1,11 +1,8 @@
-
 grep -oE 'http(s?)://www.cse.iitb.ac.in/~[a-z]+ ' $1 > url
 
-sed -r 's/\t+/ /g' $1 \
-| sed -r 's/http(s?):\/\/[^ )]+ //g' \
-| sed -r 's/\(http(s?):\/\/[^\)]+\)/( )/g' \
-| sed -r 's/ [^ ]+(\.com|\.in|\.org|\.net|\.co|\.us|\.edu|\.gov)\/[^ ]+ / /g' \
-| sed -r 's/[^\(]+(\.com|\.in|\.org|\.net|\.co|\.us|\.edu|\.gov)\/[^\)]+/ /g' \
-| sed -r 's/ +/ /g' > $1
-
-
+sed -i -r 's/\t+/ /g' $1
+sed -i -r 's/http(s?):\/\/[^ )]+ //g' $1
+sed -i -r 's/\(http(s?):\/\/[^\)]+\)/( )/g' $1
+sed -i -r 's/ [^ ]+(\.com|\.in|\.org|\.net|\.co|\.us|\.edu|\.gov)\/[^ ]+ / /g' $1
+sed -i -r 's/[^\(]+(\.com|\.in|\.org|\.net|\.co|\.us|\.edu|\.gov)\/[^\)]+/ /g' $1
+sed -i -r 's/ +/ /g' $1
